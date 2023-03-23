ab9aa8fd45e0add891e27d2196a22db69078b47c
shay20y <120938580+shay20y@users.noreply.github.com>
2023-03-23 11:28:15 +0200

    Update configRoutes.js

--- routes/configRoutes.js
+++ routes/configRoutes.js
@@ -7,4 +7,8 @@ exports.routesInit = (app) => {
   app.use("/",indexR);
   app.use("/users",usersR);
   app.use("/game/",gameR);
+  
+  app.use("/*",(req,res) =>{
+  res.status(404).json({msg:"Endpoint/page not found, 404"})
+  })
 }
